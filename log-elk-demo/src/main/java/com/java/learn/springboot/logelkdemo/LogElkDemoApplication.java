6fc7f143a84dc67010d3b36a15c87573dbdbad82
zhangpeng <zp95sky@163.com>
2021-06-15 01:19:37 +0800

    未完成

---
+++ log-elk-demo/src/main/java/com/java/learn/springboot/logelkdemo/LogElkDemoApplication.java
@@ -0,0 +1,16 @@
+package com.java.learn.springboot.logelkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 华夏紫穹
+ */
+@SpringBootApplication
+public class LogElkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogElkDemoApplication.class, args);
+    }
+
+}
